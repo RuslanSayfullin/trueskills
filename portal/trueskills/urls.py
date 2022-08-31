31dd31adfba4a9fbbd533677b0e31b135d3b2ac9
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-08-31 11:59:03 +0500

    Добавили контроллеры, шаблоны и пути для 4х страниц.

--- portal/trueskills/urls.py
+++ portal/trueskills/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('location/', location, name='location'),
+    path('rest_zone/', rest_zone, name='rest_zone'),
+    path('booking_page/', booking_page, name='booking_page'),
 ]
--- portal/trueskills/views.py
+++ portal/trueskills/views.py
@@ -1,5 +1,40 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+menu = [{'title': "Главная страница", 'url_name': 'index'},
+        {'title': "Страница с результатами поиска локаций", 'url_name': 'location'},
+        {'title': "Страница выбора конкретной зоны из свободных в локации", 'url_name': 'rest_zone'},
+        {'title': "Страница бронирования", 'url_name': 'booking_page'}
+]
 
 
 def index(request):
-    return HttpResponse("Главная страница.")
+    context = {
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'trueskills/index.html', context=context)
+
+
+def location(request):
+    context = {
+        'menu': menu,
+        'title': 'Страница с результатами поиска локаций'
+    }
+    return render(request, 'trueskills/location.html', context=context)
+
+
+def rest_zone(request):
+    context = {
+        'menu': menu,
+        'title': 'Страница с результатами поиска локаций'
+    }
+    return render(request, 'trueskills/rest_zone.html', context=context)
+
+
+def booking_page(request):
+    context = {
+        'menu': menu,
+        'title': 'Страница с результатами поиска локаций'
+    }
+    return render(request, 'trueskills/booking_page.html', context=context)
